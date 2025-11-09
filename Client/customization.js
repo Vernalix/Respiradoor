b7cff2992326529c6df7e5b811203ea55f074d67
Vernalix <fredricotorrefiel@gmail.com>
2025-11-09 18:22:05 -0400

    Update customization.js

--- Client/customization.js
+++ Client/customization.js
@@ -1,1 +1,9 @@
+// customization.js
+const colorInput = document.createElement('input');
+colorInput.type = 'color';
+document.body.appendChild(colorInput);
+
+colorInput.addEventListener('change', () => {
+    document.body.style.backgroundColor = colorInput.value;
+});
 
